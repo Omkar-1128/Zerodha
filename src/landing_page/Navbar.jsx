d7621dc33eab9095b5f8697246422437ac7e13a0
Omkar Shelke <shelkeomkar272@gmail.com>
2025-07-28 00:11:24 +0530

    Navbar and footer created

--- src/landing_page/Navbar.jsx
+++ src/landing_page/Navbar.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React from "react";
+import "./styles.css";
 
 function Navbar() {
-    return ( 
-        <>
-            <h3>Navbar</h3>
-        </>
-     );
+  return (
+    <div>
+      <nav className="navbar fixed-top navbar-expand-lg navbar-border">
+        <div className="container">
+          <a href="#">
+            <img
+              className="nav-image"
+              src="/Media/images/logo.svg"
+              alt="Logo"
+            />
+          </a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Signup
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Products
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Support
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button className="d-block nav-toggler" type="button">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
