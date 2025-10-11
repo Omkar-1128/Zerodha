542b909a12a80556d951a58b01ee53e617962a6b
Omkar Shelke <shelkeomkar272@gmail.com>
2025-10-11 23:45:35 +0530

    added data in holding and positions

--- Dashboard/src/components/Positions.jsx
+++ Dashboard/src/components/Positions.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { positions } from "../data/data";
 
 const Positions = () => {
   return (
@@ -16,6 +17,36 @@ const Positions = () => {
             <th>P&L</th>
             <th>Chg.</th>
           </tr>
+
+          {positions.map(function (position) {
+            const currValue = position.price * position.qty;
+            const isProfit = currValue - position.avg * position.qty >= 0.0;
+            const profClass = isProfit? "profit" : "loss";
+            const dayClass = position.isLoss? "loss" : "profit";
+            return (
+              <tr>
+                <td>{position.product}</td>
+                <td>{position.name}</td>
+                <td>{position.qty}</td>
+                <td>{position.avg}</td>
+                <td>{position.price}</td>
+                <td className={profClass}>{((position.price - position.avg) * position.qty).toFixed(2)}</td>
+                <td className={dayClass}>{position.day}</td>
+                <td></td>
+              </tr>
+            );
+          })}
+
+          {/* {
+        product: "CNC",
+        name: "JUBLFOOD",
+        qty: 1,
+        avg: 3124.75,
+        price: 3082.65,
+        net: "+10.04%",
+        day: "-1.35%",
+        isLoss: true,
+      } */}
         </table>
       </div>
     </>
